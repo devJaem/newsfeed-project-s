80ed4dbbc8566389acf9031fb2dbb07e10bb6f9d
seungminOh2024 <167006850+seungminOh2024@users.noreply.github.com>
2024-06-04 15:59:35 +0900

    Resolve merge conflicts

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,20 +1,15 @@
 import express from 'express';
 import authRouter from './auth.router.js';
 import usersRouter from './user.router.js';
-<<<<<<< HEAD:src/routers/route.js
 import reviewRouter from './review.router.js';
-=======
 import commentsRouter from './comments.router.js';
->>>>>>> develop:src/routers/router.js
 
 const route = express.Router();
 
 route.use('/auth', authRouter);
 route.use('/users', usersRouter);
-<<<<<<< HEAD:src/routers/route.js
 route.use('/reviews', reviewRouter);
-=======
 route.use('/comments', commentsRouter);
->>>>>>> develop:src/routers/router.js
+
 
 export default route;
